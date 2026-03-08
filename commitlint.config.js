5592be102a409e503ec3cb7290f1c790cb6bfd5c
Jinhyeok Lee <zenyr@zenyr.com>
2026-03-08 18:43:54 +0900

    chore(commitlint): tighten casing rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,15 @@
-export default {
+/** @type {import("@commitlint/types").UserConfig} */
+const config = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-case": [2, "always", "lower-case"],
+    "scope-case": [2, "always", ["lower-case", "kebab-case"]],
+    "subject-case": [
+      2,
+      "never",
+      ["sentence-case", "start-case", "pascal-case", "upper-case"],
+    ],
+  },
 };
+
+export default config;
